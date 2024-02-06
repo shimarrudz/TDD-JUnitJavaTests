154854a67aea2a4ab86d3344a40e8b46fb90ad73
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-02-06 16:52:04 -0300

    feat: finish mockito tests

--- JavaFirstTest/src/test/java/br/com/shima/MockitoTests.java
+++ JavaFirstTest/src/test/java/br/com/shima/MockitoTests.java
@@ -3,21 +3,31 @@ package br.com.shima;
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MockitoTests {
+
+    @Mock
+    List<String> lista;
+
 
     @Test
     public void primeiroTesteMockito() {
-        List<String> lista = Mockito.mock(ArrayList.class);
+        lista.add("");
+        lista.size();
 
         Mockito.when(lista.size()).thenReturn(20);
 
         int size = lista.size();
 
         Assertions.assertThat(size).isEqualTo(20);
+        Mockito.verify(lista).size();
     }
 }
